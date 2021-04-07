c2c559cd55964413a4f8fbb2f1a2f122886f9af3
ductai26998 <ductai26998@gmail.com>
2021-04-07 09:18:25 +0700

    bai tap buoi 2 js

--- homework/js_buoi2/index.js
+++ homework/js_buoi2/index.js
@@ -1,32 +1,76 @@
 // const axios = require('axios').default;
 
-getAPI();
+var error = "";
 
-async function getAPI() {
-    var text = "";
-    let result = await axios.get("https://606b20daf8678400172e5aff.mockapi.io/users/users");
+main();
 
-    for (let i = 0; i < result.data.length; i++) {
-        text += "<li>" + result.data[i].id + ". " + result.data[i].name + "</li>";
+async function main() {
+    await getAPI();
+
+    if (error != "") {
+        modal.style.display = "block";
+        document.getElementById("error").innerHTML = error;
     }
 
-    document.getElementById("data").innerHTML = text;
+    document.getElementById("submit").addEventListener("click", async function() {
+        await postAPI();
+        getAPI();
+        if (error != "") {
+            modal.style.display = "block";
+            document.getElementById("error").innerHTML = error;
+        }
+    });
+
+    document.getElementById("close").addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+async function getAPI() {
+    console.log("get");
+
+    var text = "";
+    try {
+        let result = await axios.get("https://606b20daf8678400172e5aff.mockapi.io/users/users");
+
+        for (let i = 0; i < result.data.length; i++) {
+            text += "<li>" + result.data[i].id + ". " + result.data[i].name + "</li>";
+        }
+        
+        document.getElementById("data").innerHTML = text;
+    } catch(err) {
+        error = "GET: " + err;
+    }
+
+    
+    
 }
 
 async function postAPI() {
+    console.log("post");
     let name = document.getElementById("name").value;
-    let newUser = {
-        "name": name
-    }
-    let result = await axios.post("https://606b20daf8678400172e5aff.mockapi.io/users/users", newUser);
+
+    if (name != "") {
+        let newUser = {
+            "name": name
+        }
+        try {
+            let result = await axios.post("https://606b20daf8678400172e5aff.mockapi.io/users/users", newUser);
+        } catch(err) {
+            error = "POST: " + err;
+        }
+    } else error = "Name is invalid !!!";
+    
 }
 
 
-document.getElementById("submit").addEventListener("click", async function() {
-    await postAPI();
-    getAPI();
-});
-    
+
 
 
 
